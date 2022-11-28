7db517b466d9d80b35aee597105b76433d4b5b44
jesus-orduno <jesus.orduno@gmail.com>
2022-11-27 19:06:30 -0700

    add dependency connections, connection, and init function

--- index.js
+++ index.js
@@ -1,2 +1,60 @@
 const mysql2 = require('mysql2');
 const inquirer = require('inquirer');
+const cTable = require('console.table');
+
+const db = mysql2.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'employees_db'
+  },
+  console.log(`Connected to the employees_db database.`)
+)
+
+const init = () => {
+  inquirer.prompt({
+    type: 'list',
+    name: 'start',
+    message: 'What would you like to do?',
+    choices: [
+      'View All Employees',
+      'Add Employee',
+      'Update Employee Role',
+      'View All Roles',
+      'Add Role',
+      'View All Departments',
+      'Add Department',
+      'Exit'
+    ]
+  })
+    .then((answer) => {
+      switch (answer.start) {
+        case 'View All Employees':
+          viewEmployees();
+          break;
+        case 'Add Employee':
+          addEmployee();
+          break;
+        case 'Update Employee Role':
+          updateEmployeeRole();
+          break;
+        case 'View All Roles':
+          viewRoles();
+          break;
+        case 'Add Role':
+          addRole();
+          break;
+        case 'View All Departments':
+          viewDepartments();
+          break;
+        case 'Add Department':
+          addDepartment();
+          break;
+        case 'Exit':
+          db.end();
+          break;
+      }
+    });
+}
+
